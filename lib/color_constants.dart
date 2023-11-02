f465194718cd701efa126f7d1bff3ff974ff4a73
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-02 16:50:41 +0500

    Introduced Colors

---
+++ lib/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const primary = Color(0xFF181A20);
+  static const secondary = Color(0xFF5626C4);
+  static const white = Color(0xFFFFFFFF);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paymentportal/color_constants.dart';
 import 'package:paymentportal/splashscreen_view.dart';
 
 void main() {
@@ -14,7 +15,9 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
         title: 'Payment Portal',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          scaffoldBackgroundColor: ColorConstants.primary,
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: ColorConstants.secondary),
           useMaterial3: true,
         ),
         home: SplashScreen());
